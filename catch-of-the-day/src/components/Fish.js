cfd883a5d95ca3175fa96e810632b42e1b1a5607
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-11-17 10:44:57 +0900

    15

---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Fish extends React.Component {
+    render() {
+        const { details } = this.props;
+        return (
+            <li className="menu-Fish">
+                <img src={details.image} alt={details.name} />
+                <h3 class Name="fish-name">
+                    {details.name}
+                    <span className="price">{details.price}</span>
+                </h3>
+                <p>{details.desc}</p>
+                <button>Add To Order</button>
+            </li>
+        )
+    }
+}
+
+export default Fish;
