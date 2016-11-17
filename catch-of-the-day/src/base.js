e64a51d95fb5ea349376ec7bd110ffed715c8690
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-11-17 19:28:38 +0900

    18

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyBD_45Hc-TrRfE6J-QJYOxQt4rEje-rzTE",
+    authDomain: "react-for-beginners-66158.firebaseapp.com",
+    databaseURL: "https://react-for-beginners-66158.firebaseio.com",
+});
+
+export default base;
