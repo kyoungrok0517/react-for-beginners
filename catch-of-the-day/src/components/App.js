676cb3f4b4d1927daf19c56af90e5ed2305ee202
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-11-17 20:04:34 +0900

    20

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
         super();
 
         this.addFish = this.addFish.bind(this);
+        this.updateFish = this.updateFish.bind(this);
         this.loadSamples = this.loadSamples.bind(this);
         this.addToOrder = this.addToOrder.bind(this);
         // getinitialState
@@ -61,6 +62,12 @@ class App extends React.Component {
         // this.setState({ fishes: fishes });
     }
 
+    updateFish(key, updatedFish) {
+        const fishes = {...this.state.fishes};
+        fishes[key] = updatedFish;
+        this.setState({ fishes });
+    }
+
     loadSamples() {
         this.setState({
             fishes: sampleFishes
@@ -94,7 +101,11 @@ class App extends React.Component {
                     fishes={this.state.fishes} 
                     order={this.state.order}
                     params={this.props.params} />
-                <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+                <Inventory 
+                    addFish={this.addFish} 
+                    loadSamples={this.loadSamples} 
+                    fishes={this.state.fishes}
+                    updateFish={this.updateFish} />
             </div>
         )
     }
